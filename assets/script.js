2186391901a75434c0d58f9f303fc181281c78aa
AmaalK2 <106732458+AmaalK2@users.noreply.github.com>
2023-03-29 16:18:23 +0200

    update

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,60 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+    let dots = document.querySelector(".dots"); //variable dot creer//
+
+    for (let i = 0; i < slides.length; i++){  //length  = longueur du tableau/: si i est inferieur a la longeur du tableau, / 
+	let dot = document.createElement('div'); //creation d'une div// 
+	dot.classList.add("dot"); //creation d'une class à la div//
+	dots.appendChild(dot); 
+}
+
+let arrowLeft = document.querySelector('.arrow_left'); 
+let arrowRight = document.querySelector('.arrow_right'); 
+
+arrowLeft.addEventListener('click', precedent);
+arrowRight.addEventListener('click', next);
+
+function precedent (){ 
+    currentSlide--
+     if (currentSlide < 0) {
+     	 currentSlide = slides.length - 1; 
+     	}
+    changeSlide(slides[currentSlide].image, slides[currentSlide].tagLine);       	
+}
+    	
+function next (){
+	currentSlide++
+	 if (currentSlide > 3) {
+     	 currentSlide = slides.length - 3; 
+     	}
+    changeSlide(slides[currentSlide].image, slides[currentSlide].tagLine);
+}
+
+
+
+
+let slider = document.querySelector("#banner");
+let nbr_img = slides.length;
+let currentSlide = 0; 
+
+function changeSlide (imgUrl, tagLine){
+   
+   let img = slider.querySelector('.banner-img');
+   let textContainer = slider.querySelector('p');
+
+
+   img.src="./assets/images/slideshow/" + imgUrl;
+   textContainer.innerHTML= tagLine; 
+
+}
+
+
+
+
+
+
+
+
